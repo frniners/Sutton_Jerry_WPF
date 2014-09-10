e86f37dfbe8d1a32365f7a4e1265633b3303a5cf
frniners <jerrysutton@Jerrys-MacBook-Pro.local>
2014-09-10 10:30:49 -0700

    Industry
    final configuration

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -12,13 +12,3 @@ var cogs = (+cbeginv + cops - cendinv); //calculates the above info - Cost of be
 //console.log(cogs); //prints to console
 var result = "Your Cost of Goods Sold is " + cogs + "";
 alert(result);
-
-//Cost of goods sold
-
-//var cbeginv = Number(prompt("Enter your Cost of Beginning Inventory","")) || 0; //cost of beginning inventory
-//var cops = Number(prompt("Enter your Cost of Purchases","")) || 0; //cost of purchases
-//var cendinv = Number(prompt("Enter your Cost of Ending Inventory","")) || 0; //cost of ending inventory
-
-//var cogs = cbeginv + cops - cendinv; //calculates the above info: Cost of beginning inventory + Cost of purchases - Cost of ending inventory = Costs of goods sold
-//var result = "Your Cost of Goods Sold is " + cogs + "";
-alert(result);
