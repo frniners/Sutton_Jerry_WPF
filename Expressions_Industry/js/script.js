7749e9ad57538f949b0f0f8a2c2ac1d6f43631aa
frniners <jerrysutton@Jerrys-MacBook-Pro.local>
2014-09-10 09:47:59 -0700

    Industry
    added “number” in front of prompts

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,11 +4,21 @@
 
 //Cost of goods sold
 
-var cbeginv = prompt("Enter your Cost of Beginning Inventory"); //cost of beginning inventory
-var cops = prompt("Enter your Cost of Purchases"); //cost of purchases
-var cendinv = prompt("Enter your Cost of Ending Inventory"); //cost of ending inventory
+var cbeginv = Number(prompt("Enter your Cost of Beginning Inventory")); //cost of beginning inventory
+var cops = Number(prompt("Enter your Cost of Purchases")); //cost of purchases
+var cendinv = Number(prompt("Enter your Cost of Ending Inventory")); //cost of ending inventory
 
 var cogs = (+cbeginv + cops - cendinv); //calculates the above info - Cost of beginning inventory + Cost of purchases – Cost of ending inventory = Costs of goods sold
 //console.log(cogs); //prints to console
 var result = "Your Cost of Goods Sold is " + cogs + "";
+alert(result);
+
+//Cost of goods sold
+
+//var cbeginv = Number(prompt("Enter your Cost of Beginning Inventory","")) || 0; //cost of beginning inventory
+//var cops = Number(prompt("Enter your Cost of Purchases","")) || 0; //cost of purchases
+//var cendinv = Number(prompt("Enter your Cost of Ending Inventory","")) || 0; //cost of ending inventory
+
+//var cogs = cbeginv + cops - cendinv; //calculates the above info: Cost of beginning inventory + Cost of purchases - Cost of ending inventory = Costs of goods sold
+//var result = "Your Cost of Goods Sold is " + cogs + "";
 alert(result);
