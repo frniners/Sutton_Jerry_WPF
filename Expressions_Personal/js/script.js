3ff0e6251bb5fffc7670fcc4c78231b375196a23
frniners <jerrysutton@Jerrys-MacBook-Pro.local>
2014-09-10 11:31:37 -0700

    result change

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -9,7 +9,7 @@ var prin = Number(prompt("Enter your Principal amount")); //principal amount of 
 var interest = Number(prompt("Enter your Interest rate")); //rate at which you will earn interest on your principal
 var rate = Number(prompt("Enter your rate(time)")); //time of loan
 
-var simint = (+prin * interest * rate); //calculates the above info - Simple Interest = Interest earned from principal
+var simint = (+prin * (.10 * interest) * rate); //calculates the above info - Simple Interest = Interest earned from principal
 //console.log(cogs); //prints to console
-var result = "A rough estimate on what Simple Interest you'll earn or pay " + simint + "";
+var result = " + simint + ""A rough dollar amount on what Simple Interest you'll earn or pay";
 alert(result);
