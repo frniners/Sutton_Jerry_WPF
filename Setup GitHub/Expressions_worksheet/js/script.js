8a88d97618dc4f05d7c99a5b5d8c494b3be5bc95
frniners <jerrysutton@Jerrys-MacBook-Pro.local>
2014-09-09 17:14:41 -0700

    Expressions Worksheet
    Expressions Worksheet

---
+++ Setup GitHub/Expressions_worksheet/js/script.js
@@ -0,0 +1,9 @@
+//Jerry Sutton
+//Sept 11, 2014
+//Expression Worksheet
+
+//teh average is the sum of the items divided by the number of items
+var bill = [1, 2, 3, 4, 5];
+
+var average = (bill[0] + bill[1] + bill[2] + bill[3] + bill[4])/5;
+console.log(average);
