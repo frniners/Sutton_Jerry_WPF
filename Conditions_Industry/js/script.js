5cbeab2856568ec8a10fd816524dc3ed21ed6d04
frniners <jerrysutton@Jerrys-MacBook-Pro.local>
2014-09-16 12:03:33 -0700

    coding

--- Conditions_Industry/js/script.js
+++ Conditions_Industry/js/script.js
@@ -2,14 +2,12 @@
 //Sep 18, 2014
 //Conditions Industry
 
-var time = Number(prompt("What time is it?")); //principal amount of loan or savings
-var interest = Number(prompt("Enter your Interest rate")); //rate at which you will earn interest on your principal
-var rate = Number(prompt("Enter your rate(time)"));
+var time = Number(prompt("What time is it?"));
 
 if (time < 12) {
-    console.log("Good Morning");
-} else if (time < 20) {
-    greeting = "Good day";
+    console.log("Good morning");
+} else if (time === 12) {
+    console.log("Lunch Time");
 } else {
-    greeting = "Good evening";
+    console.log("Good evening");
 }
