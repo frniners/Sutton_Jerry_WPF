14571da8be60d85e4b9dada65f2933515d0f1021
frniners <jerrysutton@Jerrys-MacBook-Pro.local>
2014-09-17 16:53:09 -0700

    final changes

--- Conditions_Wacky/js/script.js
+++ Conditions_Wacky/js/script.js
@@ -4,14 +4,13 @@
 
 var grade = prompt("What is your grade?");
 var minGrade = 70;
-var lowGrade < 69;
+var lowGrade = 69;
 
-
-
-if(minGrade < 70){
-    console.log("You have until June to get it up");
     console.log("Your grade is: " + (grade >= 90 ? "Excellent!": (grade > 70? "Average" : "Needs to be improved")) );
-}else if(lowGrade >70){
+
+if(minGrade <= 70){
+    console.log("You have until June to get it up");
+}else if(lowGrade >= 69){
     console.log("Keep up the great work!");
 }else{
     console.log("You need to input a grade");
