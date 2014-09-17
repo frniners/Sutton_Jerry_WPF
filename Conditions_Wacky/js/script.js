a2a93f2932845e4d63c58728e6248649566a5f72
frniners <jerrysutton@Jerrys-MacBook-Pro.local>
2014-09-17 12:37:10 -0700

    grades

--- Conditions_Wacky/js/script.js
+++ Conditions_Wacky/js/script.js
@@ -1,3 +1,17 @@
 //Jerry Sutton
 //Sep 18, 2014
 //Conditions Wacky
+
+var grade = prompt("What is your grade?");
+var minGrade = 70;
+
+    console.log("Your grade is: " + (grade >= 90 ? "Excellent!": (grade > 50? "Average" : "Needs to be improved")) );
+    console.log("You have until " + months + " to get it corrected");
+
+/*if(minGrade < 70){
+    console.log("You have until June to get it up");
+}else{
+    console.log("Keep up the great work!");
+}*/
+
+
