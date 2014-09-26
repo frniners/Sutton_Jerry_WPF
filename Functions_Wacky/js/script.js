f5aaee1b41753fa9be8734f7d791a2451399d82a
frniners <jerrysutton@Jerrys-MacBook-Pro.local>
2014-09-25 17:24:56 -0700

    code

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,3 +1,16 @@
 //Jerry Sutton
 //Sep 25, 2014
 //Functions Wacky
+
+var x = prompt("What was your score? \nPlease enter 1-10");
+var y = 6;
+
+/*if (x > y) {
+    result = "Great Job";
+}
+else {
+    result = I would find another Hobby;
+}*/
+
+//Below is the above code written using the ternary operator
+(result = x > y) ? console.log("Great Job") : console.log("I would find another Hobby");
