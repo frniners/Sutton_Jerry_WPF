56ee6e5362088159c213c0c1e689c6deac3eabe5
frniners <jerrysutton@Jerrys-MacBook-Pro.local>
2014-09-25 17:16:13 -0700

    code cleanup

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -2,12 +2,12 @@
 //Sep 25, 2014
 //Functions Personal
 
-
+//prompt variables to determine amounts for calculation
 var paycheck = prompt("How much was your paycheck this week? \nPlease enter the amount");
 var carpymt = prompt ("How much is your car payment this month? \nPlease enter the amount");
 var freemon = prompt ("Did you receive any extra income this week? \nPlease enter amount");
 
-//if the paycheck is less than the car payment or received extra income over the carpayment
+//if the paycheck & extra income is more than the car payment
 if(carpymt < paycheck + freemon){
     console.log("You can make your Car Payment this week!!!");
 
