01ac0bb02c06ee066893c8f021fa5bd8a044dc2e
frniners <jerrysutton@Jerrys-MacBook-Pro.local>
2014-09-25 17:33:02 -0700

    Initial Coding

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,3 +1,22 @@
 //Jerry Sutton
 //Sep 25, 2014
 //Functions Industry
+
+//Declares the function
+function myFunc(theObject) {
+    theObject.brand = "Toyota";
+}
+
+//defining
+var mycar = {
+    brand: "Honda",
+    model: "Accord",
+    year: 1998
+};
+
+//Logs Honda
+console.log(mycar.brand);
+
+//Logs Toyota
+myFunc(mycar);
+console.log(mycar.brand);
