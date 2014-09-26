0344e7408af587bf2209996e9ac43b40eb9d803f
frniners <jerrysutton@Jerrys-MacBook-Pro.local>
2014-09-25 17:34:13 -0700

    testing of Honda and Toyota

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -7,16 +7,16 @@ function myFunc(theObject) {
     theObject.brand = "Toyota";
 }
 
-
+//defining
 var mycar = {
     brand: "Honda",
     model: "Accord",
     year: 1998
 };
 
-//Logs Honda
+
 console.log(mycar.brand);
 
-//Logs Toyota
+
 myFunc(mycar);
 console.log(mycar.brand);
